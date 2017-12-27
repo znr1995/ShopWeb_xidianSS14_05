a2f112a3a038f3c81561a40b25031e1c1854a2d5
OhMyAce <3114710942@qq.com>
2017-12-27 16:13:47 +0800

    增加domain中ShopCollection类

---
+++ src/main/java/com/project_management/shoppingweb/domain/ShopCollection.java
@@ -0,0 +1,33 @@
+
+package com.project_management.shoppingweb.domain;
+
+public class ShopCollection {
+    Long id;
+    Long sellerId;
+    Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(Long sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
+
---
+++ src/main/java/com/project_management/shoppingweb/repository/ShopCollectionRepository.java
@@ -0,0 +1,4 @@
+package com.project_management.shoppingweb.repository;
+
+public interface ShopCollectionRepository extends JpaRepository<Collction,Long>{
+}
