d02526606d11644576cbde33d182d9eb1d5cf11a
JiahuanZhang <2274306468@qq.com>
2018-01-01 23:41:05 +0800

    商品详细页面完成一部分，待完善

--- src/main/java/com/project_management/shoppingweb/controller/User/Product/ProductDetailController.java
+++ src/main/java/com/project_management/shoppingweb/controller/User/Product/ProductDetailController.java
@@ -1,7 +1,9 @@
 package com.project_management.shoppingweb.controller.User.Product;
 
 import com.project_management.shoppingweb.domain.Product;
+import com.project_management.shoppingweb.domain.User;
 import com.project_management.shoppingweb.service.User.User_ProductService;
+import com.project_management.shoppingweb.service.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,7 +18,8 @@ import javax.servlet.http.HttpServletRequest;
 public class ProductDetailController {
     @Autowired
     private User_ProductService userProductService;
-
+    @Autowired
+    private UserService userService;
 
     private Logger logger = Logger.getLogger(this.getClass());
 
@@ -24,13 +27,20 @@ public class ProductDetailController {
     public  String Detail(HttpServletRequest request, Model model){
 
         String ID = request.getParameter("productId");
-        String UserID = request.getParameter("UserID");
         Product product = userProductService.findProductByProductID(Long.parseLong(ID));
         boolean isnull = false;
         if(product == null) isnull = true;
         model.addAttribute("productdetail", product);
         model.addAttribute("proisnull", isnull);
+        long UserID = Long.parseLong(request.getParameter("UserID"));
         model.addAttribute("UserID", UserID);
+        if(UserID == -1){
+            model.addAttribute("UserName", "UserName");
+        }
+        else{
+            User user = userService.findByUserId(UserID);
+            model.addAttribute("UserName", user.getUsername());
+        }
         return "/User/ProductDetail";
     }
 }
