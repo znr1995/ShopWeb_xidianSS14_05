3c289d6dbf9670b2a06a79e4fe3d0ebc6ca3d374
znr1995 <znr910870@outlook.com>
2018-01-04 20:40:10 +0800

    修改income表，数据库需要删除重建

--- src/main/java/com/project_management/shoppingweb/domain/Income.java
+++ src/main/java/com/project_management/shoppingweb/domain/Income.java
@@ -1,16 +1,57 @@
 package com.project_management.shoppingweb.domain;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "t_income")
 public class Income {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    String year;
-    String month;
-    String day;
-    Long commission;
-    Long sellerId;
-    Long sellerName;
+    private  Long id;
+    private Date date;
+    private Long commission;
+    private Long sellerId;
+    private String sellerName;
+    private String type;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Long getCommission() {
+        return commission;
+    }
+
+    public void setCommission(Long commission) {
+        this.commission = commission;
+    }
+
+    public Long getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(Long sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
